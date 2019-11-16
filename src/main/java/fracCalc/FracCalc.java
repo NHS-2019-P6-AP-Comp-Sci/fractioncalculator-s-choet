92bfb843102a951a18f361d685a227398a4171e2
s-choet <s-choet@53S500038076.it.bsd405.org>
2019-11-15 22:29:03 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -14,19 +14,26 @@ public class FracCalc {
     	
     	Scanner userInput = new Scanner(System.in);
     	boolean run = true;
-    	String sum = "";
+    	String res = "";
     	int round = 1;
     	String numinput = "";
     	
     	
     	
-    	if(run) {
+    	while (run) {
     		
-    		System.out.print("Enter calculation:" );
+    		System.out.print("Enter calculation number #" + round + ":" );
     		numinput = userInput.nextLine();
-    		System.out.println(numinput);
-    		sum = produceAnswer(numinput);
-    		System.out.println(sum);
+    		if (numinput.contains("quit")) {
+    			run = false;
+    		} else {
+    			res = produceAnswer(numinput);
+    			
+    			
+    			System.out.println(res);
+    		}
+    		
+    		
     		round++;
     	}
     	
@@ -49,8 +56,39 @@ public class FracCalc {
     	int spacenum = input.indexOf(" ");
     	String firstop = input.substring(0,spacenum);
     	String secondop = input.substring(spacenum+3,length);
+    	int opnum = secondop.indexOf("/");
+    	int secondlength = secondop.length();
+    	int wholelength = input.indexOf("_");
     	
-        return secondop;
+    	
+// Conditions for return values 	
+    	if (secondop.contains("_")) {
+    		String wholenum = secondop.substring(0,wholelength);
+			String numer = secondop.substring(wholelength +1, opnum);
+			String denom = secondop.substring(opnum+1, secondlength);
+			String outt = "whole:" + wholenum + " numerator:" + numer + " denominator:" + denom;
+        	return outt;
+    		
+    		
+    	} else {
+    		if (secondop.contains("/")) {
+    			String wholenum = "0";
+    			String numer = secondop.substring(0, opnum);
+   				String denom = secondop.substring(opnum+1, secondlength);
+   				String outt = "whole:" + wholenum + " numerator:" + numer + " denominator:" + denom;
+   	        	return outt;
+   	        	
+   			} else {
+    			String wholenum =secondop;
+    			String numer = "0";
+    			String denom = "1";
+    			String outt = "whole:" + wholenum + " numerator:" + numer + " denominator:" + denom;
+       	        return outt;
+    		}
+    			
+    	}
+    			
+    	
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
